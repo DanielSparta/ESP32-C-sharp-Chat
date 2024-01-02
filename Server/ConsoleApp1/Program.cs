9177906cd2e8c009c80ccc6e2a982a3a2a93230a
DanielSparta <111179755+DanielSparta@users.noreply.github.com>
2024-01-02 12:37:02 +0200

    Adding code

--- Server/ConsoleApp1/Program.cs
+++ Server/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 
 namespace ConsoleApp1
@@ -15,14 +17,61 @@ namespace ConsoleApp1
 
         private void Create()
         {
-            SocketServer Server = new SocketServer();
+
+            string ipaddress = CheckConfigExist();
+            SocketServer Server = new SocketServer(ipaddress);
             new Thread(new ThreadStart(() =>
             {
-                Server.AcceptClients();
+                a:
+                bool ErrorReturned = Server.AcceptClients();
+                //Error returned, create server again from other ipaddress
+                ipaddress = CheckConfigExist();
+                Server = new SocketServer(ipaddress);
+                goto a;
             })).Start();
 
             Server.OnClientConnectedEvent += NewConnection;
         }
+
+
+        private String CheckConfigExist()
+        {
+            string ipaddress = "";
+            string FilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\ipaddress.txt";
+
+            if (!File.Exists(FilePath))
+            {
+                Console.WriteLine("[i] - No saved config detected. \r\n");
+            a:
+                Console.Write("Please enter your ipv4 IpAddress: ");
+                try
+                {
+                    IPAddress ip = IPAddress.Parse(Console.ReadLine());
+                    ipaddress = ip.ToString();
+                }
+                catch
+                {
+                    Console.WriteLine("[ERROR] - invalid ipaddress.");
+                    Console.WriteLine(@"[ERROR] - IpAddress """ + ipaddress + @""" is not in valid context. it need to be taken from ""ipconfig"" command at cmd");
+                    goto a;
+                }
+
+                File.Create(FilePath).Close();
+                File.WriteAllText(FilePath, ipaddress);
+                Console.WriteLine(@"Writing ipaddress to """ + FilePath + @"""" + "\n\n\n");
+                return ipaddress;
+            }
+            else
+            {
+                ipaddress = File.ReadAllText(FilePath);
+                Console.WriteLine("[i] - Detected ipaddress saved config");
+                Console.WriteLine(@"[i] - Using default config path: """ + FilePath + @"""");
+                Console.WriteLine("[i] - Using " + ipaddress + " as default ipaddress.");
+                Console.WriteLine(@"[i] - If you wish to change the IpAddress, You need to open the saved text file from path and change its ipaddress." + "\n\n\n");
+                return ipaddress;
+            }
+        }
+
 
         private void NewConnection(Socket ClientSocket)
         {
--- Server/ConsoleApp1/SocketServer.cs
+++ Server/ConsoleApp1/SocketServer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Sockets;
 using System.Net;
+using System.Reflection;
 
 namespace ConsoleApp1
 {
@@ -7,16 +8,26 @@ namespace ConsoleApp1
     {   
         public delegate void ClientConnected(Socket ConnectedClient);
         public event ClientConnected OnClientConnectedEvent;
-
         private Socket socketserver;
+        private bool SocketCreateError = false;
 
-        public SocketServer()
+        public SocketServer(string ipaddress)
         {
             this.socketserver = server();
-            IPEndPoint iep = SetIpDetail("172.0.2.131", 81);
+            IPEndPoint iep = SetIpDetail(ipaddress, 81);
             Console.WriteLine("[i] Server created: " + iep as string);
-            socketserver.Bind(iep);
-            socketserver.Listen(1000);
+            try
+            {
+                socketserver.Bind(iep);
+                socketserver.Listen(1000);
+            }
+            catch
+            {
+                Console.WriteLine(@"[ERROR] - IpAddress is not valid.");
+                Console.WriteLine(@"[ERROR] - IpAddress """ + ipaddress + @""" is not in valid context. it need to be taken from ""ipconfig"" command at cmd");
+                File.Delete(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\ipaddress.txt");
+                this.SocketCreateError = true;
+            }
         }
 
 
@@ -35,9 +46,12 @@ namespace ConsoleApp1
 
 
         
-        public void AcceptClients()
+        public bool AcceptClients()
         {
-            while(true)
+            if (SocketCreateError)
+                return true;
+
+            while (true)
             {
                 Socket client = this.socketserver.Accept();
                 OnClientConnectedEvent.Invoke(client);
